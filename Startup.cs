dee549871dfe3c174d38e64230b264977b3a6797
Siddhartha <sbanjee.banerjee@gmail.com>
2020-09-17 23:18:13 +0530

    Swagger Added

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace Dockersampleconfig
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,10 @@ namespace Dockersampleconfig
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            { c.SwaggerEndpoint("/swagger/v1/swagger.json", "swaggertst"); });
 
             app.UseHttpsRedirection();
 
